245ddebb63bf51b112eb26eee3ac5286fa1045e0
Florian Seefried <fseefried@ad.ipag.wzw.tum.de>
2018-04-17 12:00:45 +0200

    v1 - it works

---
+++ server.R
@@ -0,0 +1,160 @@
+require(shiny)
+require(shinyjs)
+
+
+source("./helpers/plotfunction.R")
+
+options(shiny.maxRequestSize=1000*1024^2)
+
+
+shinyServer(function(input, output , session) {
+  
+  value <- reactiveValues()
+  
+  data <- NULL
+  
+  plotfun_Env <- new.env()
+  
+  plotfun <- NULL
+  
+  selected <- NULL
+  
+  counter <- NULL
+  
+  observeEvent(input$file , {
+    
+   value$data <<- try(read.csv(input$file$datapath , header = input$import.header , sep = input$import.sep , quote = input$import.quote , stringsAsFactors = F , row.names = NULL , na.strings = c("" , "NA")))
+   
+  #column is called JTarget due to the fact that R has a nasty autocomplition for subsetting lists with $ and to avoid to break it JTarget is Choosen as uniqe
+   
+   if(is.null(value$data$JTarget)){
+   
+   value$data <- cbind(value$data , JTarget = rep(NA , times = nrow(value$data)) )
+   selected <<- sample(1:nrow(value$data) , 1)
+   counter <<- 0
+   }else{
+     
+     remaining <- as.numeric(rownames(value$data[is.na(value$data$JTarget),]))
+     selected <<- sample(remaining , 1)
+     counter <<- nrow(value$data)-length(remaining)
+     
+     
+   }
+   
+   
+  
+  })
+  
+  output$progress <- renderUI({
+    
+    if(is.null(value$data)){
+      
+      return(NULL)
+      
+    }else{
+      
+      return(paste(counter , "/" ,  nrow(value$data)))
+      
+    }
+    
+    
+  })
+
+ 
+  
+  output$plotout <- renderPlot({
+    validate(need(!is.null(value$data) , "No data set selected")
+             #TOREMOVE: is needed if upload plotfun should be activated
+             #,need(!is.null(plotfun) , "No plotfunction available")
+             )
+    
+    #the secound argument is here specific for the data
+    #TOREMOVE: is needed if upload plotfun should be activated and replace the other plotfun
+    #plotfun(data = value$data ,selected = selected)
+    plot_CurvePlot(data = value$data ,selected = selected )
+    
+    
+  })
+  
+  
+
+  
+observeEvent(input$decision,{
+  #TOREMOVE: is needed if upload plotfun should be activated and replace the other plotfun
+  #req(!is.null(plotfun))
+
+  if(!is.null(value$data)){
+
+    if(input$decision[1] == 39){
+
+      value$data[selected , "JTarget"] <- TRUE
+      remaining <- as.numeric(rownames(value$data[is.na(value$data$JTarget),]))
+      selected <<- sample(remaining , 1)
+      counter <<- counter+1
+
+    }else if(input$decision[1] == 37){
+      value$data[selected , "JTarget"] <- FALSE
+      remaining <- as.numeric(rownames(value$data[is.na(value$data$JTarget),]))
+      selected <<- sample(remaining , 1)
+      counter <<- counter+1
+
+    }
+
+  
+  }
+
+output$Save <- downloadHandler(filename = paste0(input$file$name , "_judged") , content = function(file){
+  
+  write.csv(value$data , file = file, row.names = F)
+  
+})
+
+
+
+
+})
+
+  
+  
+ 
+observe({
+  
+  f <- input$plotScript$datapath
+  
+  if(!is.null(f)){
+    rm(list = ls(envir = plotfun_Env) ,envir =  plotfun_Env)
+    source(f , local = plotfun_Env)
+    
+    
+  }
+  
+  if(length(grep("^plot_" , ls(envir = plotfun_Env))) == 1){
+    
+    plotfun <<- plotfun_Env[[ (grep("^plot_" , ls(envir = plotfun_Env) , value = T))]]
+  }
+  
+  
+
+  
+  output$plotfuncode <- renderPrint({
+    
+    validate(need(!is.null(plotfun), message = "No plot function avaiable!\n"))
+    
+    plotfun
+    
+    
+  })
+  
+  
+})
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+})
---
+++ ui.R
@@ -0,0 +1,94 @@
+require(shiny)
+
+
+shinyUI(
+  fluidPage(
+    
+    tags$script('
+              $(document).on("keydown", function (e) {
+              Shiny.onInputChange("decision", [e.which,e.timeStamp]);
+              });
+              '),  
+    tags$style(type="text/css", "
+                                       #loadmessage {
+                              position: fixed;
+                              top: 50px;
+                              left: 0px;
+                              width: 100%;
+                              padding: 5px 0px 5px 0px;
+                              text-align: center;
+                              font-weight: bold;
+                              font-size: 100%;
+                              color: #000000;
+                              background-color: #FF0000;
+                              z-index: 105;
+                              }
+                              "),
+    conditionalPanel(condition="$('html').hasClass('shiny-busy')",
+                     
+                     tags$div("Loading...",id="loadmessage")),
+    #fluidRow( column(2 , div(h1("cindeR" )) , style="color:red;text-align:center") ),#, column(11 , img(src='logo.png', align = "right" , height = "100px" , width = "80px"))),
+    useShinyjs(),
+    
+    
+    navbarPage(div("cindeR" , style="color:red;text-align:center"),
+               tabPanel("Evaluation",
+                        sidebarPanel(
+                          
+                          wellPanel(
+                          
+                          shiny::fileInput(inputId = "file" , label = "Upload a csv file to judge") ,
+                          checkboxInput("import.header", "Header", TRUE),
+                          
+                          selectInput("import.sep", "Separator", selected = ",",
+                                      
+                                      choices = c(Comma = ",", Semicolon = ";", Tab = "\t")),
+                          
+                          selectInput("import.quote", "Quote", selected = '"',
+                                      
+                                      choices = c(None = "", "Double Quote" = '"', "Single Quote" = "'"))
+                          
+                          
+                          
+                        ),
+                          
+                          wellPanel(
+                            h4("Instructions"),
+                            icon("toggle-left") , ": negative" ,
+                            
+                            icon("toggle-right") , ": positive"),
+                          br(),
+                          
+                          wellPanel(
+                            h4("Progress"),
+                            uiOutput("progress")
+                          ),
+                          
+                          downloadButton("Save" , "Download current progress")
+                          
+                          
+                          
+                        ),
+                        
+                        mainPanel(
+                          
+                          shiny::plotOutput("plotout" ,height = "600px")
+                            
+                        )
+                        
+               ),
+               tabPanel("Plot function",
+                        sidebarPanel( width = 3 ,
+                                      fileInput("plotScript" , label = "Select a plot script")
+                        ),
+                        mainPanel(width = 9 ,
+                                  h4("Plot function"),
+                                  verbatimTextOutput("plotfuncode")))
+               
+               
+               
+    )
+    
+    
+  )
+)
